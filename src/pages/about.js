638117d3917027e8c5155bfbc777d1034d02681a
Lewis Parnell <lewis.g.parnell@gmail.com>
2021-11-17 16:21:09 -0800

    Create website

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Us</title>
+            <h1>About Positive Systems</h1>
+            <p>With over 1 year of experience in the event production industry, we know how to get a video wall put up. (Additional costs may apply to removing said wall.)</p>
+        </main>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Positive Systems</title>
+      <h1>Positive Systems</h1>
+      <Link to="/about">About</Link>
+      <p>We provide LED video wall rentals, event management, and home improvement at highly competitive prices!</p>
+    </main>
+  )
+}
+
+export default IndexPage
