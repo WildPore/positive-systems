d0c2c6e0d153a00121f0d8ac0f7418b6c0383dbf
lgparnell <lewis.g.parnell@gmail.com>
2022-02-01 14:00:11 -0800

    Update: really I don't know what's happened

---
+++ src/pages/equipment.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const EquipmentPage = () => {
+  return (
+    <Layout pageTitle="Our Equipment">
+      <p>We have a large inventory of high-quality video walls.</p>
+      <h2>Absen PL3.9</h2>
+      <p>
+        At a 3.9mm pixel-pitch, the Absen PL3.9 is our highest resolution wall,
+        rated for indoors and outdoors, has seen many different events around
+        the PNW.
+      </p>
+      <h2>Absen AT5</h2>
+      <h2>MJ4</h2>
+    </Layout>
+  );
+};
+
+export default EquipmentPage;
